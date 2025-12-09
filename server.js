e88be282f4e12e577165aca2578aa8b6a7ef3e3e
sathwikv2005 <sathwikv2005@gmail.com>
2025-12-09 23:38:31 +0530

    add express server

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-require('dotenv').config()
-const express = require('express')
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = express()
 const port = process.env.PORT || 9005
-const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get(/.*/, (req, res) => {
-	return res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 app.listen(port, () => {
